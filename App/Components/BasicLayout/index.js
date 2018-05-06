35a3c1d8025bdfb1bdac23021b628762c62a2c83
fancn <fanyj@yikeer.com>
2018-05-06 11:27:19 +0800

    basic layout

---
+++ App/Components/BasicLayout/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const BasicLayout = () => (
+  <View style={{flex: 1}}>
+    <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+    <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+    <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+  </View>
+)
+
+export default BasicLayout
+
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './App/Components/BasicLayout';
 
 AppRegistry.registerComponent('rn_theme', () => App);
